f16df7b736027b3ffc0a5d439a2eb6efe66490e7
Kevin Cho <kccho2254@gmail.com>
2021-03-06 19:53:50 -0600

    f

---
+++ react-demo/src_navbar/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PortfolioContainer from "../../../UTA-AUS-FSF-PT-10-2020-U-C/19-React/01-Activities/23-Stu_PupsterApp/pupster/src/components/PortfolioContainer";
+
+function App() {
+  return <PortfolioContainer />;
+}
+
+export default App;
